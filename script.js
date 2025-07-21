fed25cc1736edca4cafcffbaa7266fa833f156a4
muriel <murielehlinger@gmail.com>
2025-07-21 08:41:31 +0200

    textes défilants OK

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 // texte défilant
 function disparaitre() {
-    document.getElementById('marqueeContainer').classList.add('disparu');
+    // ça cache le container en desktop si présent
+    let containerDeskop = document.getElementById('marqueeContainer');
+    if(containerDeskop) {containerDeskop.classList.add('disparu')};
+
+    // ça cache le container en mobile si présent
+    let containerMobile = document.getElementById('marqueeContainerMobile');
+    if(containerMobile) {containerMobile.classList.add('disparu')};
 }
+// Ou, si tu veux que **n'importe où** sur la page fasse disparaître les deux :
 document.body.addEventListener('click', disparaitre);
 
-// texte défilant_mobile
-function disparaitre() {
-    document.getElementById('marqueeContainerMobile').classList.add('disparu');
-}
-document.body.addEventListener('click', disparaitre);
+
+
+// function disparaitre() {
+//     document.getElementById('marqueeContainer').classList.add('disparu');
+// }
+// document.body.addEventListener('click', disparaitre);
+
+// // texte défilant_mobile
+// function disparaitre() {
+//     document.getElementById('marqueeContainerMobile').classList.add('disparu');
+// }
+// document.body.addEventListener('click', disparaitre);
+
+
 // a propos
 let bouton = document.getElementById('afficher_a_propos');
 let imageContainer = document.getElementById('image-container');
