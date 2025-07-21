01ddabfd743467a2770bb57ebde64f3dfa801217
muriel <murielehlinger@gmail.com>
2025-07-21 20:08:01 +0200

    vérif mobile étape de W

--- script.js
+++ script.js
@@ -4,27 +4,53 @@ function disparaitre() {
     let containerDeskop = document.getElementById('marqueeContainer');
     if(containerDeskop) {containerDeskop.classList.add('disparu')};
 
+    let arrow = document.getElementById('arrow');
+    if (arrow) {arrow.classList.add('disparu')};
+
     // ça cache le container en mobile si présent
     let containerMobile = document.getElementById('marqueeContainerMobile');
     if(containerMobile) {containerMobile.classList.add('disparu')};
 }
+function handleScroll() {
+    if (window.scrollY > 100) {
+        arrow.classList.add('hidden');
+        // On retire le scrollListener, la flèche ne reviendra plus
+        window.removeEventListener('scroll', handleScroll);
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 100) { // change le seuil selon ton besoin
+//         arrow.classList.add('hidden');
+//     } else {
+//         arrow.classList.remove('hidden');
+//     }
+// });
+
+// js
+// const arrow = document.getElementById('arrow');
+
+
 // Ou, si tu veux que **n'importe où** sur la page fasse disparaître les deux :
 document.body.addEventListener('click', disparaitre);
 
 
 
-// function disparaitre() {
-//     document.getElementById('marqueeContainer').classList.add('disparu');
-// }
-// document.body.addEventListener('click', disparaitre);
+// links N A V
+const links = document.querySelectorAll('.nav-link');
 
-// // texte défilant_mobile
-// function disparaitre() {
-//     document.getElementById('marqueeContainerMobile').classList.add('disparu');
-// }
-// document.body.addEventListener('click', disparaitre);
-
-
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+    // Vérifie la largeur de fenêtre pour n'agir qu'en mobile...
+    
+        links.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    
+    // Sinon (desktop), tu peux ignorer ou garder le comportement
+    });
+})
 // a propos
 let bouton = document.getElementById('afficher_a_propos');
 let imageContainer = document.getElementById('image-container');
